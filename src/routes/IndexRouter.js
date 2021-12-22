81c6c5c7abb0c83302e0cf3241fac6e0ef79d57e
Martin Cappi <mcappi91@gmail.com>
2021-12-21 21:35:48 -0300

    IndexRouter back up

--- src/routes/IndexRouter.js
+++ src/routes/IndexRouter.js
@@ -1,14 +1,23 @@
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import AppRouter from "./AppRouter";
-import { reauthenticate } from "../redux/slices/authSlice";
-import AuthRouter from "./AuthRouter";
+// import { Switch, Route, Redirect, useHistory } from "react-router-dom"; // "react-router-dom": "5.3.0",
+import { Routes, Route } from "react-router-dom"; // "react-router-dom": "^6.2.1",
+import { useDispatch, useSelector } from "react-redux";
+import { reauthenticate, selectToken } from "../redux/slices/authSlice";
 import { getToken } from "../helpers/localToken";
+// import AppRouter from "./AppRouter";
+// import AuthRouter from "./AuthRouter";
+// import Header from "../components/Header";
+// import Footer from "../components/Footer";
+import Layout from "./Layout";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import RequireAuth from "./RequireAuth";
+import TaskPage from "../pages/TaskPage";
 
 const IndexRouter = () => {
+  // const token = useSelector(selectToken);
   const localToken = getToken();
   const dispatch = useDispatch();
 
@@ -21,32 +30,32 @@ const IndexRouter = () => {
   }, []);
 
   return (
-    <Switch>
-      <Route
-        path="/auth"
-        component={(props) =>
-          !localToken ? <AuthRouter {...props} /> : <Redirect to="/" />
-        }
-      />
-      <Route
-        path="/"
-        component={(props) =>
-          localToken ? <AppRouter {...props} /> : <Redirect to="/auth/login" />
-        }
-      />
-    </Switch>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/auth/register" element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <TaskPage />
+            </RequireAuth>
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
 export default IndexRouter;
 
-// React-router-dom @6
+// "react-router-dom": "^6.2.1",
 // const IndexRouter = () => {
 //   return (
 //     <Routes>
 //       <Route element={<Layout />}>
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
+//         <Route path="/auth/login" element={<LoginPage />} />
+//         <Route path="/auth/register" element={<RegisterPage />} />
 //         <Route
 //           path="/"
 //           element={
@@ -59,3 +68,27 @@ export default IndexRouter;
 //     </Routes>
 //   );
 // };
+
+//  "react-router-dom": "5.3.0",
+/* <>
+<Header />
+<Switch>
+  <Route
+    path="/auth"
+    component={(props) =>
+      !localToken ? <AuthRouter {...props} /> : <Redirect to="/" />
+    }
+  />
+  <Route
+    path="/"
+    component={(props) =>
+      localToken ? (
+        <AppRouter {...props} />
+      ) : (
+        <Redirect to="/auth/login" />
+      )
+    }
+  />
+</Switch>
+<Footer />
+</> */
