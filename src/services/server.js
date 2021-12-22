89a8e78d1b0b1c49eeab69a4c43883e1ab413294
Martin Cappi <mcappi91@gmail.com>
2021-12-22 08:55:16 -0300

    moved server base url to .env

--- src/services/server.js
+++ src/services/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://localhost:3002/"
+  baseURL: process.env.REACT_APP_SERVER_BASE_URL
 });
 
 export default apiService;
