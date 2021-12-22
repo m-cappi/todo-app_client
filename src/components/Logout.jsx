c568570d76910305b9422ac0352efe86e49ed234
Martin Cappi <mcappi91@gmail.com>
2021-12-22 08:54:44 -0300

    logout navigation

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { removeToken } from "../helpers/localToken";
 import { logout } from "../redux/slices/authSlice";
 
 const Logout = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     removeToken();
     dispatch(logout());
+    navigate("/auth/login");
   };
+
   return (
     <button type="button" onClick={handleLogout} className="logout">
       Cerrar Sesion
