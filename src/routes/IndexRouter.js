0d9e4c8bc393dbb74530b13347faf63ed32bc961
Martin Cappi <mcappi91@gmail.com>
2021-12-21 21:50:57 -0300

    update react-router-dom to v6 & refactor IndexRouter

--- src/routes/IndexRouter.js
+++ src/routes/IndexRouter.js
@@ -1,23 +1,17 @@
 /* eslint-disable react/no-unstable-nested-components */
-/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from "react";
-// import { Switch, Route, Redirect, useHistory } from "react-router-dom"; // "react-router-dom": "5.3.0",
 import { Routes, Route } from "react-router-dom"; // "react-router-dom": "^6.2.1",
-import { useDispatch, useSelector } from "react-redux";
-import { reauthenticate, selectToken } from "../redux/slices/authSlice";
+import { useDispatch } from "react-redux";
+import { reauthenticate } from "../redux/slices/authSlice";
 import { getToken } from "../helpers/localToken";
-// import AppRouter from "./AppRouter";
-// import AuthRouter from "./AuthRouter";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
 import Layout from "./Layout";
+import RequireAuth from "./RequireAuth";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import RequireAuth from "./RequireAuth";
 import TaskPage from "../pages/TaskPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 const IndexRouter = () => {
-  // const token = useSelector(selectToken);
   const localToken = getToken();
   const dispatch = useDispatch();
 
@@ -42,53 +36,10 @@ const IndexRouter = () => {
             </RequireAuth>
           }
         />
+        <Route path="/*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
 };
 
 export default IndexRouter;
-
-// "react-router-dom": "^6.2.1",
-// const IndexRouter = () => {
-//   return (
-//     <Routes>
-//       <Route element={<Layout />}>
-//         <Route path="/auth/login" element={<LoginPage />} />
-//         <Route path="/auth/register" element={<RegisterPage />} />
-//         <Route
-//           path="/"
-//           element={
-//             <RequireAuth>
-//               <TaskPage />
-//             </RequireAuth>
-//           }
-//         />
-//       </Route>
-//     </Routes>
-//   );
-// };
-
-//  "react-router-dom": "5.3.0",
-/* <>
-<Header />
-<Switch>
-  <Route
-    path="/auth"
-    component={(props) =>
-      !localToken ? <AuthRouter {...props} /> : <Redirect to="/" />
-    }
-  />
-  <Route
-    path="/"
-    component={(props) =>
-      localToken ? (
-        <AppRouter {...props} />
-      ) : (
-        <Redirect to="/auth/login" />
-      )
-    }
-  />
-</Switch>
-<Footer />
-</> */
